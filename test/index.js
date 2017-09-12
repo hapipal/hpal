1eeb7686addebe03991c6f6d2fd72a64ca8e3b7f
Devin Ivy <devin@bigroomstudios.com>
2017-09-11 23:35:28 -0400

    Refactor for testing, write a couple initial tests

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@
 // Load modules
 
 const Lab = require('lab');
+const RunUtil = require('./run-util');
 
 // Test shortcuts
 
@@ -14,4 +15,37 @@ const expect = Lab.expect;
 
 const internals = {};
 
-describe('paldo', () => null);
+describe('paldo', () => {
+
+    describe('CLI', () => {
+
+        it('outputs help [-h, --help]', () => {
+
+            return RunUtil.cli(['-h'])
+                .then((output) => {
+
+                    expect(output).to.contain('Usage: paldo ???');
+
+                    return RunUtil.cli(['--help']);
+                })
+                .then((output) => {
+
+                    expect(output).to.contain('Usage: paldo ???');
+                });
+        });
+    });
+
+    describe('bin', () => {
+
+        it('passes through argvs.', () => {
+
+            return RunUtil.bin(['-h']) // "-h" is just an example argv
+                .then((result) => {
+
+                    expect(result.code).to.equal(0);
+                    expect(result.errorOutput).to.equal('');
+                    expect(result.output).to.contain('Usage: paldo ???');
+                });
+        });
+    });
+});
---
+++ test/run-util.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const ChildProcess = require('child_process');
+const Path = require('path');
+const Stream = require('stream');
+const Paldo = require('..');
+
+exports.bin = (argv, cwd) => {
+
+    return new Promise((resolve, reject) => {
+
+        const path = Path.join(__dirname, '..', 'bin', 'paldo');
+        const cli = ChildProcess.spawn('node', [].concat(path, argv), { cwd: cwd || __dirname });
+
+        let output = '';
+        let errorOutput = '';
+        let combinedOutput = '';
+
+        cli.stdout.on('data', (data) => {
+
+            output += data;
+            combinedOutput += data;
+        });
+
+        cli.stderr.on('data', (data) => {
+
+            errorOutput += data;
+            combinedOutput += data;
+        });
+
+        cli.once('close', (code, signal) => {
+
+            if (signal) {
+                return reject(new Error(`Unexpected signal: ${signal}`));
+            }
+
+            return resolve({ output, errorOutput, combinedOutput, code, signal });
+        });
+    });
+};
+
+exports.cli = (argv, cwd) => {
+
+    const out = new Stream.PassThrough();
+
+    let output = '';
+
+    out.on('data', (data) => {
+
+        output += data;
+    });
+
+    return Promise.resolve()
+        .then(() => Paldo.start({ argv, out, cwd: cwd || __dirname }))
+        .then(() => output);
+};
